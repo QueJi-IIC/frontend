99a0510233c015564d327645e8fc3ba37e8f2241
Yashraj Pahwa <59451283+yashrajpahwa@users.noreply.github.com>
2024-11-15 15:40:18 +0530

    added firebase store add capability (stash popped)

---
+++ queji/src/data/store.jsx
@@ -0,0 +1,53 @@
+import { collection, doc, setDoc } from "firebase/firestore";
+import { firestore } from "../configs/firebase";
+
+const getStoreData = (
+  capacity,
+  tracking_object,
+  name,
+  desc,
+  created_by,
+  area
+) => {
+  return {
+    capacity,
+    tracking_object_type: tracking_object,
+    name,
+    desc,
+    created_by,
+    area,
+  };
+};
+
+const tracking_objects = ["Person", "Car"];
+
+const addStore = async (
+  setError,
+  capacity,
+  tracking_object,
+  name,
+  desc,
+  created_by,
+  area
+) => {
+  try {
+    // Create a new document reference with an auto-generated ID
+    const newStoreRef = doc(collection(firestore, "stores"));
+
+    // Set the document data
+    await setDoc(
+      newStoreRef,
+      getStoreData(capacity, tracking_object, name, desc, created_by, area)
+    );
+
+    // Optionally, you can clear the error if the operation is successful
+    setError(null);
+  } catch (error) {
+    // Handle any errors that occur during the Firestore operation
+    console.error("Error adding store: ", error);
+    setError("Failed to add store. Please try again.");
+    throw new Error("Error adding store");
+  }
+};
+
+export { addStore };
