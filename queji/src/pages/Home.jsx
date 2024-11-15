5f620a63e43b0ccd67bb3b8f44bfc06485542c90
sz05 <shreyans0528@gmail.com>
2024-11-15 16:40:34 +0530

    added distanceMapping to home page for now

--- queji/src/pages/Home.jsx
+++ queji/src/pages/Home.jsx
@@ -2,8 +2,26 @@ import React, { useState, useEffect } from "react";
 import { Button } from "react-bootstrap";
 import { Link } from "react-router-dom";
 import Background from "../components/background";
+import axios from "axios";
 
 const Home = () => {
+  const fetchDistanceMatrix = async () => {
+    try {
+      const response = await axios.post(
+        "http://localhost:5500/api/distance-matrix",
+        {
+          origins: [[78.9629, 20.5937]], // Example coordinates
+          destinations: [[74.006, 40.7128]],
+        }
+      );
+      console.log("Distance Matrix:", response.data);
+    } catch (error) {
+      console.error("Error fetching distance matrix:", error);
+    }
+  };
+
+  //   fetchDistanceMatrix();
+
   return (
     <div className="min-h-screen bg-gray-900 text-white flex flex-col items-center justify-center">
       <Background />
