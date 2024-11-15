cb349b10def9cf10e02e4b924424bb64b6e8d55c
sz05 <shreyans0528@gmail.com>
2024-11-15 13:01:01 +0530

    code with errors

--- queji/src/App.jsx
+++ queji/src/App.jsx
@@ -1,6 +1,6 @@
 import React from "react";
-import { Route } from "react-router-dom";
-import { Routes } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
+// import { Routes } from "react-router-dom";
 
 const App = () => {
   return (
--- queji/src/main.jsx
+++ queji/src/main.jsx
@@ -3,11 +3,12 @@ import { createRoot } from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./index.css";
 import App from "./App.jsx";
+import { Router } from "react-router-dom";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <BrowserRouter>
+    <Router>
       <App />
-    </BrowserRouter>
+    </Router>
   </StrictMode>
 );
