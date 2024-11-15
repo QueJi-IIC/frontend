8dc4c45bd41553658b7efc9f7fadf95b2d15d747
Yashraj Pahwa <59451283+yashrajpahwa@users.noreply.github.com>
2024-11-15 15:29:29 +0530

    added logout button

--- queji/src/App.jsx
+++ queji/src/App.jsx
@@ -8,8 +8,29 @@ import Login from "./pages/Login";
 import MyStores from "./pages/MyStores";
 
 const App = () => {
+  function Header() {
+  const [user] = useAuthState(auth);
+
+  const handleLogout = () => {
+    signOut(auth);
+  };
+
+  return (
+    <div className="fixed z-50 top-0 right-0 p-4">
+      {user && (
+        <button
+          onClick={handleLogout}
+          className="bg-red-500 text-white px-4 py-2 rounded"
+        >
+          Logout
+        </button>
+      )}
+    </div>
+  );
+}
   return (
     <>
+    <Header />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/customer-auth" element={<Customer />} />
