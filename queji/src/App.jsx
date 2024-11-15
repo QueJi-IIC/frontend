6c215c319dc60696e625931660e298d1aa132b4f
Yashraj Pahwa <59451283+yashrajpahwa@users.noreply.github.com>
2024-11-15 13:04:11 +0530

    error fixed

--- queji/src/App.jsx
+++ queji/src/App.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
-// import { Routes } from "react-router-dom";
 
 const App = () => {
   return (
--- queji/src/main.jsx
+++ queji/src/main.jsx
@@ -1,9 +1,8 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./index.css";
 import App from "./App.jsx";
-import { Router } from "react-router-dom";
+import { BrowserRouter as Router } from "react-router-dom";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
